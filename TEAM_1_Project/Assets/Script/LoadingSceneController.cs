00aaefcb919e9dea6b51a93b22b7449a06e32d77
여기원 <ygw8253@gmail.com>
2022-03-13 01:11:09 +0900

    2022.03.13
    씬체인저 추가 및 로딩창 구현

---
+++ TEAM_1_Project/Assets/Script/LoadingSceneController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    static string nextScene;
+
+    [SerializeField] Image ProgressBar;
+
+    public static void LoadScene(string SceneName)
+    {
+        nextScene = SceneName;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LoadingScene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneProcess());
+    }
+
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0f;
+        while(!op.isDone)
+        {
+            yield return null;
+            if(op.progress <0.9f)
+            {
+                ProgressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                ProgressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
+                if(ProgressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+
+        }
+
+    }
+}
