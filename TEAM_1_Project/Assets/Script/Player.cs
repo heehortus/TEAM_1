39a052d983cbb3abb0f45b189d8cfb76c699d059
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-03-14 21:58:08 +0900

    수정

--- TEAM_1_Project/Assets/Script/Player.cs
+++ TEAM_1_Project/Assets/Script/Player.cs
@@ -19,26 +19,23 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         player = this;
+
+        player._unitDic.Add("SeedUnit1", true);
+        player._unitDic.Add("SeedUnit2", false);
+        player._unitDic.Add("SeedUnit3", false);
+
+        player._unitDic.Add("BoomUnit1", true);
+        player._unitDic.Add("BoomUnit2", false);
+        player._unitDic.Add("BoomUnit3", false);
+
+        player._unitDic.Add("StealerUnit1", true);
+        player._unitDic.Add("StealerUnit2", false);
+        player._unitDic.Add("StealerUnit3", false);
+
+        player._unitDic.Add("Unit", false); // 일단 UnitFactory에 있는 유닛들만 임시로 보유하도록 설정
     }
     static public Player GetInstance()
     {
-        if(player != null) {
-            player = new Player();
-
-            player._unitDic.Add("SeedUnit1", true);
-            player._unitDic.Add("SeedUnit2", false);
-            player._unitDic.Add("SeedUnit3", false);
-
-            player._unitDic.Add("BoomUnit1", true);
-            player._unitDic.Add("BoomUnit2", false);
-            player._unitDic.Add("BoomUnit3", false);
-
-            player._unitDic.Add("StealerUnit1", true);
-            player._unitDic.Add("StealerUnit2", false);
-            player._unitDic.Add("StealerUnit3", false);
-
-            player._unitDic.Add("Unit", false); // 일단 UnitFactory에 있는 유닛들만 임시로 보유하도록 설정
-        }
         return player;
     }
     void Update()
