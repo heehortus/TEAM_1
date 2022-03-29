d751b2678516f6638f6b6c39d5b640487c505a0f
kjhcocomi <981028_@naver.com>
2022-03-29 12:39:45 +0900

    Audio 쉽게 접근할 수 있도록 수정, Bgm, Effect 틀 수 있는 함수 생성
    Main Scene 설정에서 사운드 테스트 임시로 추가

--- TEAM_1_Project/Assets/Script/Audio/Audio.cs
+++ TEAM_1_Project/Assets/Script/Audio/Audio.cs
@@ -4,8 +4,58 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
-    public AudioSource bgm;
-    public AudioSource effect;
+    static Audio s_instance;
+    public static Audio Instance { get { return s_instance; } }
+    public static AudioSource BGM { get { return s_instance.bgm; } }
+    public static AudioSource EFFECT { get { return s_instance.effect; } }
+    [SerializeField]
+    AudioSource bgm;
+    [SerializeField]
+    AudioSource effect;
     public float bgmSound = 0.5f;
     public float effectSound = 0.5f;
+    private void Awake()
+    {
+        s_instance = this;
+    }
+    public static void ChangeBgmSound(float value)
+    {
+        s_instance.bgmSound = value;
+        s_instance.bgm.volume = value;
+    }
+    public static void ChangeEffectSound(float value)
+    {
+        s_instance.effectSound = value;
+        s_instance.effect.volume = value;
+    }
+    public static void PlayBgm(string path)
+    {
+        AudioClip bgmClip = Resources.Load<AudioClip>($"Sounds/Bgm/{path}");
+        if (bgmClip == null)
+        {
+            Debug.Log($"Failed to Load : {path}");
+            return;
+        }
+        s_instance.bgm.Stop();
+        s_instance.bgm.clip = bgmClip;
+        s_instance.bgm.Play();
+    }
+    public static void PlayEffect(string path)
+    {
+        AudioClip effectClip = Resources.Load<AudioClip>($"Sounds/Effect/{path}");
+        if (effectClip == null)
+        {
+            Debug.Log($"Failed to Load : {path}");
+            return;
+        }
+        s_instance.effect.PlayOneShot(effectClip);
+    }
+    public static void StopBgm()
+    {
+        s_instance.bgm.Stop();
+    }
+    public static void StopEffect()
+    {
+        s_instance.effect.Stop();
+    }
 }
--- TEAM_1_Project/Assets/Script/UI/UI_MainScene_Setting.cs
+++ TEAM_1_Project/Assets/Script/UI/UI_MainScene_Setting.cs
@@ -5,30 +5,32 @@ using UnityEngine.UI;
 
 public class UI_MainScene_Setting : MonoBehaviour
 {
-    new Audio audio;
     public Scrollbar bgm;
     public Scrollbar effect;
     void Start()
     {
-        audio = GameObject.Find("Audio").GetComponent<Audio>();
-        bgm.value = audio.bgmSound;
-        effect.value = audio.effectSound;
-        bgm.onValueChanged.AddListener(ChangeBgm);
-        effect.onValueChanged.AddListener(ChangeEffect);
+        bgm.value = Audio.Instance.bgmSound;
+        effect.value = Audio.Instance.effectSound;
+        bgm.onValueChanged.AddListener(Audio.ChangeBgmSound);
+        effect.onValueChanged.AddListener(Audio.ChangeEffectSound);
+
+        // tmp
+        Audio.PlayBgm("TestBgm");
     }
 
     public void PushBackButton()
     {
+        // tmp
+        Audio.StopBgm();
+        Audio.StopEffect();
+        //
+
         Destroy(gameObject);
     }
-    void ChangeBgm(float value)
+
+    //tmp
+    public void TestSound()
     {
-        audio.bgmSound = value;
-        audio.bgm.volume = audio.bgmSound;
-    }
-    void ChangeEffect(float value)
-    {
-        audio.effectSound = value;
-        audio.effect.volume = audio.effectSound;
+        Audio.PlayEffect("TestEffect");
     }
 }
