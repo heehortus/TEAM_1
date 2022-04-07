d4b27fc02e4f15d6a6a1899c5b9de761e64a4e94
dongcheolpark <head02325@gmail.com>
2022-04-07 15:28:03 +0900

    각종 ui및 버튼 추가

--- TEAM_1_Project/Assets/Script/UI/UI_Setting.cs
+++ TEAM_1_Project/Assets/Script/UI/UI_Setting.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class UI_Setting : MonoBehaviour
 {
     public void PushBattleSettingButton()
     {
-        GameObject _uiSettingMenu = Instantiate(ResourceManager.LoadUI("UI_BattleScene_Setting"));
+        GameObject _uiSettingMenu = Instantiate(ResourceManager.LoadUI("UI_LobbyScene_Setting"));
         if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "BattleScene")
             _uiSettingMenu.transform.SetParent(GameManager.sceneManager.UI_Parent.transform);
         _uiSettingMenu.name = "UI_BattleScene_Setting";
